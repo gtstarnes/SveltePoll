97a42cd8f62a42d699d33f10d32d09ed0acbc239
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-28 17:09:30 -0500

    added tabs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+    import Tabs from '../Components/Tabs.svelte'
 
+    const tabs = ["Polls", "Add New Poll"]
+    let activeTab = "Polls";
+
+    const changeTab = (e: CustomEvent<string>) => {
+        activeTab = e.detail;
+    }
 </script>
 
 
 <main>
     <h1>Svelte Poll</h1>
+    <Tabs {tabs} {activeTab} on:changeTab={changeTab}/>
 </main>
 
 <style>
