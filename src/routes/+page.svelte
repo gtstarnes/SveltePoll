421dc59e6bd64d47844e3d70ee2357f96efad12d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-28 18:01:13 -0500

    added add poll form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
     import Tabs from '../Components/Tabs.svelte'
+    import AddPollForm from '../Components/AddPollForm.svelte';
+    import type {PollType} from '../types/types';
 
     const tabs = ["Polls", "Add New Poll"]
     let activeTab = "Polls";
+    let polls: PollType[] = [];
 
     const changeTab = (e: CustomEvent<string>) => {
         activeTab = e.detail;
+    }
+
+    const addPoll = (e: CustomEvent) => {
+        polls = [e.detail, ...polls]
+        console.log(polls);
+        activeTab = "Polls";
     }
 </script>
 
@@ -16,7 +25,8 @@
     {#if activeTab === "Polls"}
         <p>Polls</p>
     {:else}
-        <p>Add New Poll form</p>
+        <h4>Add a new Poll</h4>
+        <AddPollForm on:addPoll={addPoll} />
     {/if}
 </main>
 
