c9566c57c12c9e65e5c5b1d05dbd873cf47382ab
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-28 19:06:14 -0500

    created poll list

---
+++ src/Components/polls/pollsList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import type { PollType } from "../../types/types"
+    import PollDetails from "./pollDetails.svelte";
+    import Card from "./card.svelte";
+
+    export let polls: PollType[] = []
+</script>
+
+<div>
+    {#each polls as poll (poll.id)}
+        <Card>
+            <PollDetails {poll} on:vote />
+        </Card>
+    {/each}
+</div>
+
+<style>
+
+</style>
