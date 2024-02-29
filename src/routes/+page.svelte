d5827dba779dfd3297bca6fd273c4a1a50c7e21d
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-28 19:06:44 -0500

    added polls functions / components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,42 @@
 <script lang="ts">
     import Tabs from '../Components/Tabs.svelte'
     import AddPollForm from '../Components/AddPollForm.svelte';
-    import type {PollType} from '../types/types';
+    import type {PollType, VoteType} from '../types/types';
+	import PollsList from '../Components/polls/pollsList.svelte';
 
     const tabs = ["Polls", "Add New Poll"]
-    let activeTab = "Polls";
+    let activeTab: string;
     let polls: PollType[] = [];
+
+    if (polls.length === 0) {
+        activeTab = "Add New Poll";
+    } else {
+        activeTab = "Polls";
+    }
 
     const changeTab = (e: CustomEvent<string>) => {
         activeTab = e.detail;
     }
 
-    const addPoll = (e: CustomEvent) => {
+    const addPoll = (e: CustomEvent<PollType>) => {
         polls = [e.detail, ...polls]
         console.log(polls);
         activeTab = "Polls";
+    }
+
+    const handleVote = (e: CustomEvent<VoteType>) => {
+        const {option, id} = e.detail;
+        polls = polls.map(poll => {
+            if (poll.id === id) {
+                if(option === 'a'){
+                    return {...poll, voteA: poll.voteA + 1}
+                }
+                if(option === 'b'){
+                    return {...poll, voteB: poll.voteB + 1}
+                }
+            }
+            return poll;
+        })
     }
 </script>
 
@@ -23,7 +45,11 @@
     <h1>Svelte Poll</h1>
     <Tabs {tabs} {activeTab} on:changeTab={changeTab}/>
     {#if activeTab === "Polls"}
-        <p>Polls</p>
+        {#if polls.length === 0}
+            <p>There are no polls to show</p>
+        {:else}
+            <PollsList {polls} on:vote={handleVote} />
+        {/if}
     {:else}
         <h4>Add a new Poll</h4>
         <AddPollForm on:addPoll={addPoll} />
