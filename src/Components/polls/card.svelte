1ed3a7f242f116890be1dfc28306ac3ecd0e6e06
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-02-28 19:05:45 -0500

    created card for polls

---
+++ src/Components/polls/card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<div>
+    <slot></slot>
+</div>
+
+<style>
+    div {
+        border: 4px solid white;
+        padding: 5px;
+        filter: drop-shadow(10px, 10px 2px gold);
+        border-radius: 2px;
+    }
+</style>
