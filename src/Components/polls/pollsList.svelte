e1b4b15384ba35792b51066ef2106111968609ba
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-01 23:19:30 -0500

    added delete button

--- src/Components/polls/pollsList.svelte
+++ src/Components/polls/pollsList.svelte
@@ -4,7 +4,19 @@
     import Card from "./card.svelte";
     import PollStore from "../../stores/PollStore";
 	
-    let polls = $PollStore;
+    $: polls = $PollStore;
+
+    const handleDelete = (id: number | undefined) => {
+        if (typeof id === 'undefined') {
+            return;
+        }
+        PollStore.update(polls => {
+            return polls.filter(poll => {
+                return poll.id != id;
+            })
+        })
+
+    }
 
 </script>
 
@@ -13,14 +25,27 @@
         <p>There are no polls to show</p>
     {:else}
         {#each polls as poll (poll.id)}
-            <Card>
-                <PollDetails pollData={poll} />
-            </Card>
+                <Card>
+                    <PollDetails pollData={poll} />
+                    <div class="delete">
+                        <button on:click={() => handleDelete(poll.id)}>Delete Poll</button>
+                    </div>
+                </Card>
         {/each}
     {/if}
 </div>
 
 <style>
+    .delete {
+        width: 100%;
+        margin-top: 20px;
+    }
+    .delete button {
+        background-color: red;
+        height: 40px;
+        width: 10%;
+        cursor: pointer;
+    }
     .pollList {
         display: flex;
         flex-direction: column;
