930cf8a17e43786ef4d7f62fd1447dd14d33f582
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-01 22:31:03 -0500

    added everything to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,37 @@
 <script lang="ts">
     import Tabs from '../Components/Tabs.svelte'
     import AddPollForm from '../Components/AddPollForm.svelte';
-    import type {PollType, VoteType} from '../types/types';
+    import type {PollType} from '../types/types';
 	import PollsList from '../Components/polls/pollsList.svelte';
 	import Header from '../Components/header.svelte';
 	import Footer from '../Components/footer.svelte';
     import PollStore from '../stores/PollStore';
 
-    const tabs = ["Polls", "Add New Poll"]
+    // Tab
+    const tabs: string[] = ["Poll List", "Add New Poll"]
     let activeTab: string;
-
-    if ($PollStore.length === 0) {
-        activeTab = "Add New Poll";
+    if ($PollStore.length === 0){
+        activeTab = tabs[1];
     } else {
-        activeTab = "Polls";
+        activeTab = tabs[0];
     }
-
     const changeTab = (e: CustomEvent<string>) => {
         activeTab = e.detail;
     }
-
-    const addPoll = (e: CustomEvent<PollType>) => {
-        activeTab = "Polls";
+    const addPoll = () => {
+        activeTab = tabs[0]
     }
-
 </script>
 
 
 <main>
     <Header />
-    <Tabs {tabs} {activeTab} on:changeTab={changeTab}/>
-    {#if activeTab === "Polls"}
+    <Tabs {tabs} {activeTab}  on:changeTab={changeTab}/>
+    {#if activeTab === tabs[0]}
         <PollsList />
     {:else}
         <h4>Add New Poll</h4>
-        <AddPollForm on:addPoll={addPoll} />
+        <AddPollForm on:addPoll={addPoll}/>
     {/if}
     <Footer />
 </main>
