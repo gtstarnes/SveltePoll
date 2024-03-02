34a03c285eea2bd3af72c0d693f9995b34728dbd
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-01 22:30:47 -0500

    created details for polls

--- src/Components/polls/pollDetails.svelte
+++ src/Components/polls/pollDetails.svelte
@@ -2,50 +2,58 @@
     import type { PollType } from "../../types/types";
     import PollStore from "../../stores/PollStore";
 
-    export let poll: PollType;
+    export let pollData: PollType;
+    $: poll = $PollStore.find(p => {
+        return p.id === pollData.id;
+    })
+
     
-    let pollData: PollType | undefined;
-    $: pollData = $PollStore.find(p => p.id === poll.id);
+    $: totalVotes = (poll?.voteA ?? 0) + (poll?.voteB ?? 0);
+    $: percentA = Math.floor((poll?.voteA ?? 0) / totalVotes * 100 );
+    $: percentB = Math.floor((poll?.voteB ?? 0) / totalVotes * 100 );
 
-    $: totalVotes = (pollData?.voteA ?? 0) + (pollData?.voteB ?? 0);
-    $: percentA = Math.floor((pollData?.voteA ?? 0) / totalVotes * 100);
-    $: percentB = Math.floor((pollData?.voteB ?? 0) / totalVotes * 100);
-
-    const handleVote = (option: string, id: number) => {
+    const handleVote = (option: string, id: number | undefined) => {
+        if (typeof poll === "undefined") {
+            return
+        }
         PollStore.update(polls => {
             return polls.map(poll => {
                 if (poll.id === id) {
                     if (option === 'a') {
-                        return {...poll, voteA: poll.voteA + 1};
-                    }
+                        return {...poll, voteA: poll.voteA + 1}
+                    } 
                     if (option === 'b') {
-                        return {...poll, voteB: poll.voteB + 1};
+                        return {...poll, voteB: poll.voteB + 1}
                     }
                 }
-                return poll;
-            });
-        });
+                return poll
+            })
+        })
     }
-
-
 </script>
 
 <div class="poll">
-    <h2>{poll.question}</h2>
+    <h2>{poll?.question}</h2>
     <div class="buttons">
-        <button class="percent-A" style="width: {percentA}%" on:click={()=>handleVote('a', poll.id)}>{poll.optionA}</button>
-        <button class="percent-B" style="width: {percentB}%" on:click={()=>handleVote('b', poll.id)}>{poll.optionB}</button>
+        <button on:click={() => handleVote('a', poll?.id)}>
+            <div class="percent percent-A" style="width: {percentA}%"></div>
+            <span>{poll?.optionA}</span>
+        </button>
+        <button on:click={() => handleVote('b', poll?.id)}>
+            <div class="percent percent-B" style="width: {percentB}%"></div>
+            <span>{poll?.optionB}</span>
+        </button>
     </div>
     <div class="totals">
-        <p>Votes for A: {pollData?.voteA ?? 0}</p>
+        <p>Votes for A: {poll?.voteA ?? 0}</p>
         <p>Total Votes: {totalVotes}</p>
-        <p>Votes for B: {pollData?.voteB ?? 0}</p>
+        <p>Votes for B: {poll?.voteB ?? 0}</p>
     </div>
 </div>
 
 <style>
     button {
-        background: none;
+        background: rgba(128, 128, 128, 0.5);
         color: inherit;
         border:none;
         padding: 0;
@@ -53,17 +61,26 @@
         outline: inherit;
         cursor: pointer;
         height: 50px;
+        width: 100%;
+        position: relative;
+        margin: 10px auto;
     }
     .buttons {
         margin-bottom: 20px;
         display: flex;
         flex-direction: column;
         width: 50%;
-        gap: 40px
+        gap: 10px
+    }
+    .percent {
+        height: 100%;
+        position: absolute;
+        top: 0.1px;
+        box-sizing: border-box;
     }
     .percent-A {
         border-left: 4px solid red;
-        background-color: rgba(217,27,66,0.2);
+        background-color: rgba(217,27,66,0.2);        
     }
     .percent-B {
         border-left: 4px solid green;
@@ -73,6 +90,10 @@
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+    span {
+        display: inline-block;
+        padding: 10px 20px;
     }
     .totals {
         display: flex;
