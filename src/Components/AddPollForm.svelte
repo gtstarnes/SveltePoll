ecf35e96abaaeddaf60cf42aad36f5804db6da47
gtstarnes <108637294+gtstarnes@users.noreply.github.com>
2024-03-08 18:55:20 -0500

    recreated

--- src/Components/AddPollForm.svelte
+++ src/Components/AddPollForm.svelte
@@ -2,64 +2,61 @@
     import PollStore from "../stores/PollStore";
 	import { createEventDispatcher } from "svelte";
 
-    let pollData = {question: '', optionA: '', optionB: ''}
+    let fields = {question: '', optionA: '', optionB: ''}
     let error = {question: '', optionA: '', optionB: ''}
     let valid = false;
     const dispatch = createEventDispatcher();
 
+    const resetPoll = () => {
+        fields = {question: '', optionA: '', optionB: ''}
+        error = {question: '', optionA: '', optionB: ''}
+    }
     const submitPoll = () => {
         valid = true
-        if (pollData.question.length < 10) {
+        if (fields.question.length < 10){
             valid = false
             error.question = 'Question must be longer than 10 characters'
         } else {
             error.question = ''
         }
-        if (pollData.optionA.length === 0 ) {
+        if (fields.optionA.length === 0){
             valid = false
             error.optionA = 'Option is required'
         } else {
             error.optionA = ''
         }
-        if (pollData.optionB.length === 0 ) {
+        if (fields.optionB.length === 0){
             valid = false
             error.optionB = 'Option is required'
         } else {
             error.optionB = ''
         }
 
-        if (valid) {
-            const poll = {
-                ...pollData, id: Math.random(), voteA: 0, voteB: 0,
-            }
+        if(valid){
+            const poll = {...fields, voteA: 0, voteB: 0, id: Math.random()}
             PollStore.update(polls => {
                 return [poll, ...polls]
             })
-            dispatch('addPoll')
+            dispatch('addPoll');
             resetPoll();
         }
-    }
-
-    const resetPoll = () => {
-        pollData = {question: '', optionA: '', optionB: ''}
-        error = {question: '', optionA: '', optionB: ''}
     }
 </script>
 
 <form on:submit|preventDefault={submitPoll}>
     <div class="form-field">
         <label for="question">Question</label>
-        <input id="question" name="question" type="text" placeholder="Question" bind:value={pollData.question}  />
+        <input name="question" id="question" type="text" placeholder="Question" bind:value={fields.question} />
         <p class="error">{error.question}</p>
     </div>
     <div class="form-field">
         <label for="optionA">Option A</label>
-        <input id="optionA" name="optionA" type="text" placeholder="Option A" bind:value={pollData.optionA}  />
+        <input name="optionA" id="optionA" type="text" placeholder="Option A" bind:value={fields.optionA} />
         <p class="error">{error.optionA}</p>
     </div>
     <div class="form-field">
         <label for="optionB">Option B</label>
-        <input id="optionB" name="optionB" type="text" placeholder="Option B" bind:value={pollData.optionB}  />
+        <input name="optionB" id="optionB" type="text" placeholder="Option B" bind:value={fields.optionB} />
         <p class="error">{error.optionB}</p>
     </div>
     <div>
